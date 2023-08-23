57c3e374d50d8d1bc56cf728b4e0b1cd7bbaf570
G <0020992@gmail.com>
2023-08-23 18:58:54 +0300

    Merge remote-tracking branch 'origin/develop' into develop

--- str_func.py
+++ str_func.py
@@ -2,3 +2,21 @@ def to_uppercase(input_str):
     """: Строка в верхнем регистре.
     """
     return input_str.upper()
+
+
+def capitalize_first_letters(input_str: str) -> str:
+    """
+    Преобразует первую букву каждого слова строки в заглавную.
+
+    Args:
+    input_str (str): Входная строка.
+
+    Returns:
+    str: Преобразованная строка.
+
+    Пример:
+    >>> capitalize_first_letters("привет мир")
+    'Привет Мир'
+    """
+
+    return ' '.join(word.capitalize() for word in input_str.split())
