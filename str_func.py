7086a1f44f34fa8f2de4d8f38cae7a1f695b1507
G <0020992@gmail.com>
2023-08-23 17:33:09 +0300

    Merge remote-tracking branch 'origin/develop' into develop

--- str_func.py
+++ str_func.py
@@ -1,11 +1,4 @@
 def to_uppercase(input_str):
-    """
-    Преобразует входную строку в верхний регистр.
-
-    Args:
-        input_str (str): Входная строка.
-
-    Returns:
-        str: Строка в верхнем регистре.
+    """: Строка в верхнем регистре.
     """
     return input_str.upper()
